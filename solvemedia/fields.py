b913f49e1d22969872034af176fb39c78a2c06f4
whitehat2k13 <whitehat2k9@gmail.com>
2013-07-15 19:22:45 -0500

    Fix handling of None values

--- solvemedia/fields.py
+++ solvemedia/fields.py
@@ -5,27 +5,27 @@ from solvemedia.widgets import SolveMediaCaptcha
 import sys
 
 class SolveMediaCaptchaField(forms.CharField):
-    def __init__(self, *args, **kwargs):
-        super(SolveMediaCaptchaField, self).__init__(*args, **kwargs)
-        if 'label' not in kwargs:
-            self.label = "Solve the puzzle"
-        self.required = True
-        self.widget = SolveMediaCaptcha()
+	def __init__(self, *args, **kwargs):
+		super(SolveMediaCaptchaField, self).__init__(*args, **kwargs)
+		if 'label' not in kwargs:
+			self.label = "Solve the puzzle"
+		self.required = True
+		self.widget = SolveMediaCaptcha()
 
-    def __getRequestIP(self): # wtfhax https://github.com/praekelt/django-recaptcha/blob/master/captcha/fields.py
-        f = sys._getframe()
-        while f:
-            if 'request' in f.f_locals:
-                request = f.f_locals['request']
-                if request:
-                    remoteIP = request.META.get('REMOTE_ADDR', '')
-                    return remoteIP
-            f = f.f_back
+	def _getRequestIP(self): # wtfhax https://github.com/praekelt/django-recaptcha/blob/master/captcha/fields.py
+		f = sys._getframe()
+		while f:
+			if 'request' in f.f_locals:
+				request = f.f_locals['request']
+				if request:
+					remoteIP = request.META.get('REMOTE_ADDR', '')
+					return remoteIP
+			f = f.f_back
 
-    def clean(self, val):
-        super(SolveMediaCaptchaField, self).clean(val['response'])
-        sm = SolveMedia(settings.SM_CKEY, settings.SM_VKEY, settings.SM_HKEY)
-        result = sm.check_answer(self.__getRequestIP(), val['challenge'].encode('utf-8'), val['response'].encode('utf-8'))
-        if not result['is_valid']:
-            raise forms.ValidationError("Error: " + result['error'].capitalize())
-        return val['response']
+	def clean(self, val):
+		super(SolveMediaCaptchaField, self).clean(val['response'])
+		sm = SolveMedia(settings.SM_CKEY, settings.SM_VKEY, settings.SM_HKEY)
+		result = sm.check_answer(self._getRequestIP(), val['challenge'].encode('utf-8'), val['response'].encode('utf-8'))
+		if not result['is_valid']:
+			raise forms.ValidationError("Error: " + result['error'].capitalize())
+		return val['response']
--- solvemedia/widgets.py
+++ solvemedia/widgets.py
@@ -4,9 +4,9 @@ from django.utils.safestring import mark_safe
 from solvemedia import SolveMedia
 
 class SolveMediaCaptcha(forms.widgets.Widget):
-    def render(self, name, value, attrs=None):
-        sm = SolveMedia(settings.SM_CKEY, settings.SM_VKEY, settings.SM_HKEY)
-        return mark_safe(sm.get_html() + "<br>")
+	def render(self, name, value, attrs=None):
+		sm = SolveMedia(settings.SM_CKEY, settings.SM_VKEY, settings.SM_HKEY)
+		return mark_safe(sm.get_html() + "<br>")
 
-    def value_from_datadict(self, data, files, name):
-        return {'challenge': data.get('adcopy_challenge'), 'response': data.get('adcopy_response')}
+	def value_from_datadict(self, data, files, name):
+		return {'challenge': data.get('adcopy_challenge', ''), 'response': data.get('adcopy_response', '')}
